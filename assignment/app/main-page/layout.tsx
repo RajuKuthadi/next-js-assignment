c6ea602153245934b3cb9c92ea607c8bf0f1123a
RajuKuthadi <rajukuthadi420@gmail.com>
2024-11-09 13:57:26 +0530

    Initial setup

---
+++ assignment/app/main-page/layout.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+export default function MainPageLayout({children}:{children: React.ReactNode}) {
+  return (
+    <div>
+        <div>test</div>
+        {children}
+    </div>
+  )
+}
